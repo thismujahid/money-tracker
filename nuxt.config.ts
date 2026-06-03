74164e6d1d342e79564de4002807937cc64eff24
Muhammad mujahid <mohamed.mojahead@gmail.com>
2026-06-03 15:45:53 +0300

    fix pwa icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,66 +38,71 @@ export default defineNuxtConfig({
       "id": "/?source=pwa",
       "icons": [
         {
-          "src": "icons/icon-48x48.png",
+          "src": "/icon.svg",
+          "sizes": "any",
+          "type": "image/png"
+        },
+        {
+          "src": "/icons/icon-48x48.png",
           "sizes": "48x48",
           "type": "image/png"
         },
         {
-          "src": "icons/icon-72x72.png",
+          "src": "/icons/icon-72x72.png",
           "sizes": "72x72",
           "type": "image/png"
         },
         {
-          "src": "icons/icon-96x96.png",
+          "src": "/icons/icon-96x96.png",
           "sizes": "96x96",
           "type": "image/png"
         },
         {
-          "src": "icons/icon-128x128.png",
+          "src": "/icons/icon-128x128.png",
           "sizes": "128x128",
           "type": "image/png"
         },
         {
-          "src": "icons/icon-144x144.png",
+          "src": "/icons/icon-144x144.png",
           "sizes": "144x144",
           "type": "image/png"
         },
         {
-          "src": "icons/icon-152x152.png",
+          "src": "/icons/icon-152x152.png",
           "sizes": "152x152",
           "type": "image/png"
         },
         {
-          "src": "icons/icon-192x192.png",
+          "src": "/icons/icon-192x192.png",
           "sizes": "192x192",
           "type": "image/png"
         },
         {
-          "src": "icons/icon-256x256.png",
+          "src": "/icons/icon-256x256.png",
           "sizes": "256x256",
           "type": "image/png"
         },
         {
-          "src": "icons/icon-384x384.png",
+          "src": "/icons/icon-384x384.png",
           "sizes": "384x384",
           "type": "image/png"
         },
         {
-          "src": "icons/icon-512x512.png",
+          "src": "/icons/icon-512x512.png",
           "sizes": "512x512",
           "type": "image/png"
         }
       ],
       "screenshots": [
         {
-          "src": "screenshot-desktop.png",
+          "src": "/screenshot-desktop.png",
           "sizes": "1280x720",
           "type": "image/png",
           "form_factor": "wide",
           "label": "الواجهة الرئيسية للتطبيق على الكمبيوتر"
         },
         {
-          "src": "screenshot-mobile.png",
+          "src": "/screenshot-mobile.png",
           "sizes": "390x844",
           "type": "image/png",
           "form_factor": "narrow",
